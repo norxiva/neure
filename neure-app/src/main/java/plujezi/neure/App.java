38f7fb961e36a29193d1ec3a3a203e9ecf74e5ce
tomenttosa <tomenttosa@foxmail.com>
2017-04-24 19:33:16 +0800

    add config server.

--- neure-app/src/main/java/plujezi/neure/App.java
+++ neure-app/src/main/java/plujezi/neure/App.java
@@ -4,10 +4,12 @@ package plujezi.neure;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.support.SpringBootServletInitializer;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.ImportResource;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 @EnableTransactionManagement
 @ImportResource(value = {"classpath:dubbo.xml"})
 public class App extends SpringBootServletInitializer {
